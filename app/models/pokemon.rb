16628fd43522f0f2aac354d2ef178c3a7443b688
cmkoller <cmkoller01@gmail.com>
2014-12-27 19:05:51 -0500

    Fixed loose 'else' statement, mistakenly deleted and then re-added JS

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -2,20 +2,19 @@ class Pokemon
   attr_reader :id, :name, :image_url
 
   def initialize(id)
-      @id = id
-      if id <= 151
-        info = JSON.parse(Pokegem.get 'pokemon', @id)
-        @name = info["name"]
-        @image_url = "http://img.pokemondb.net/sprites/black-white/anim/normal/#{@name.downcase}.gif"
-      else
-        @name = "MissingNo"
-        @image_url = "/assets/missingno.png"
-      end
+    @id = id
+    if id <= 151
+      info = JSON.parse(Pokegem.get 'pokemon', @id)
+      @name = info["name"]
+      @image_url = "http://img.pokemondb.net/sprites/black-white/anim/normal/#{@name.downcase}.gif"
     else
-      @id, @name, @image_url = nil
+      @name = "MissingNo"
+      @image_url = "/assets/missingno.png"
+    end
   end
 
   def self.search(id)
+    # binding.pry
     if id.to_s.length == 3 && id.to_s =~ /^[0-9]{0,3}$/
       Pokemon.new(id.to_i)
     end
